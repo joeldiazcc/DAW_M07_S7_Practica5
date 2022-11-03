5276cad8cfce02da062ad73979329d3f945ef3fc
Joel Felipe Díaz Carissimi <64970845+joeldiazcc@users.noreply.github.com>
2022-11-03 20:27:24 +0100

    form add

---
+++ form_add.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<!-- Formulari per afegir-->
+<form method="post" action="add_product.php">
+<p>Name: <input type="text" name="name" value="Oro"/></p>
+<p>Description: <input type="text" name="description" value="Preciado"/></p>
+<p>Price: <input type="number" name="price" value="34"/></p>
+<p>Quantity: <input type="number" name="q_sold" value="1"/></p>
+<p><input type="submit"/></p>
+</form>
