9f5db70f78326b51fc6860dc0c6da46ad24c04a8
Ivan Petrov <ipetrov20@jaumebalmes.net>
2022-11-03 20:45:11 +0100

    Ahora funciona el insert

--- add_product.php
+++ add_product.php
@@ -1,18 +1,14 @@
 <?php
 
-include 'index.php';
-  
-
-    if (!isset($_POST['nada']) ) {
-        exit();
-    }
+include 'index.php'; 
 
     $name = $_POST['name'];
     $description = $_POST['description'];
     $price = $_POST['price'];
     $quantity = $_POST['q_sold'];
 
-    $sql_insert = $myCon->prepare("INSERT INTO ddbb(´name´, ´description´, ´price´, ´q_sold´) VALUES (?,?,?,?);");
+    //HACEOS LA QUERY DONDE INSERTAMOS LOS DATOS
+    $sql_insert = $myCon->prepare("INSERT INTO ddbb(name, description, price, q_sold) VALUES(?,?,?,?);");
     $insert = $sql_insert->execute([$name,$description,$price,$quantity]);
 
     ?>
